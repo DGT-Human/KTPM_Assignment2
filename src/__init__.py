7272e3e680b4ea3b2737e11534bb03a8e52cf167
GF63-DGT\DGT <dugiatien14@gmail.com>
2024-10-26 17:11:18 +0700

    up: Update

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .driver.driver import Driver
+from .pages.base_page import BasePage
+from .pages.login_page import LoginPage
+from .pages.register_page import RegisterPage
+from .tests.test_login import TestLogin
+from .tests.test_register import TestRegister
--- src/tests/test_login.py
+++ src/tests/test_login.py
@@ -1,7 +1,6 @@
 import time
-from Assignment.src.pages.base_page import BasePage
-from Assignment.src.driver.driver import Driver
-from Assignment.src.pages.login_page import LoginPage
+from src.driver.driver import Driver
+from src.pages.login_page import LoginPage
 
 
 class TestLogin(Driver):
@@ -46,3 +45,13 @@ class TestLogin(Driver):
         message = login_page.get_error_message()
         time.sleep(3)
         assert "Warning: No match for E-Mail Address and/or Password." in message
+
+    def test_logout(self, driver):
+        login_page = LoginPage(driver)
+        login_page.go_to_login()
+        login_page.login("b@b.com", "12345")
+        time.sleep(3)
+        login_page.logout()
+        time.sleep(3)
+        message = ''
+        assert "" in message
